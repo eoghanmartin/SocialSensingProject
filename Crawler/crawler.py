ae9505eca46bf7f15f6ddbb3534d12a726c813ee
Kurt Davis <kurt.davis13@gmail.com>
2017-03-02 20:13:16 -0500

    Add initial results

--- Crawler/crawler.py
+++ Crawler/crawler.py
@@ -26,6 +26,7 @@ class textListener(StreamListener):
 				for word in self.keywords: 
 					if not data.text.encode('ascii', 'ignore').lower().find(word) == -1:
 						#print(data.text)
+						#print("Filter word detected!")
 						print json.dumps(data._json)
 						self.tweet_count = self.tweet_count + 1
 						return True
@@ -58,11 +59,7 @@ access_token_secret = str(keys[7].replace('\n', ' ').replace('\r', '').replace('
 sys.stdout = open(output_file_name, 'w')
 
 # Initialize listener. NOTE: arg1 = number of tweets before terminating, arg2 = list of substrings to select
-<<<<<<< HEAD
 listener1 = textListener(1000, ['trump', 'clinton', 'election', 'potus', 'president', 'white house', 'whitehouse'])
-=======
-listener1 = textListener(1, ['trump', 'clinton'])
->>>>>>> 37a072ae9c9276a2e04a876c559ee7e28ef1c9c3
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
@@ -70,9 +67,5 @@ api = tweepy.API(auth)
 
 # Start listener. NOTE: Hard code desired location here
 stream1 = Stream(auth, listener1)
-<<<<<<< HEAD
-stream1.filter(locations=[-85.7867431641, 42.7696989802, -85.3143310547, 43.292700534])
+stream1.filter(locations=[-86.9177277088,41.7286433379,-82.4194335937,45.8019991667])
 #stream1.filter(locations=[-86.33,41.63,-86.20, 41.74])
-=======
-stream1.filter(locations=[-86.33,41.63,-86.20, 41.74])
->>>>>>> 37a072ae9c9276a2e04a876c559ee7e28ef1c9c3
